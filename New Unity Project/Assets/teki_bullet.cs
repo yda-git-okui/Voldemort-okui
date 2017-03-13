16a6ceace4271e5cf0ce23d9dfbf7875c6f89a62
DA\nwuser <yugoyugo19970618@softbank.ne.jp>
2017-03-13 16:37:01 +0900

    ゆうぎょ

---
+++ New Unity Project/Assets/teki_bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teki_bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity
+		= new Vector2(0, -5.0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
