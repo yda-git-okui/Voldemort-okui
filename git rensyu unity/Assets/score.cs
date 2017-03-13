ce91be4de96c36dd8b297f9e997786a0c4057845
DA\nwuser <kakumeino.turugi@gmail.com>
2017-03-13 15:47:45 +0900

    haikei 画像は容易した  scrollまだ 　スコアこれから

---
+++ git rensyu unity/Assets/score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+
+
+    Text score_text;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
