e9a4f5b940d4e4d5f27d2f30b010571fae1318ab
nwuser <nwuser@da.iwasaki.ac.jp>
2017-03-13 16:29:37 +0900

    ドロシーはクソ　：　敵の生成　：　tsuyoshi

---
+++ git rensyu unity/Assets/script/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public GameObject item;
+    int cnt;
+    int obj_x;
+    Vector3 Object = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+        cnt = 0;
+        Object.x = (Random.Range(0, 10) + 1) * 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        cnt++;
+		if(cnt >= 80)
+        {
+            Instantiate(item, transform.position + Object, transform.rotation);
+            cnt = 0;
+            Object.x = (Random.Range(0, 10) + 1) * 2;
+        }
+	}
+}
